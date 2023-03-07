7c5fb6c7f3000f46398dad6021652db65e1353ec
Christopher Harrison <geektrainer@github.com>
2023-03-07 15:00:58 -0700

    Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import dbConnect from "../lib/dbConnect";
 import Pet from "../models/Pet";
 
 // TODO: Import Hours component
-import Hours from "../components/Hours";
 
 const Index = ({ pets }) => {
 
@@ -11,7 +10,6 @@ const Index = ({ pets }) => {
     <>
 
       {/* TODO: Display Hours component */}
-      <Hours />
 
       {/* Create a card for each pet */}
       {pets.map((pet) => (
